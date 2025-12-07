562e0b5aca3c9259004cea033fca67008782a4c8
Rafael <redenemeit@gmail.com>
2025-12-07 17:17:22 -0300

    primeiro commit

---
+++ 010.py
@@ -0,0 +1,4 @@
+tabuada = int(input('Digite o número para aparecer a tabuada: '))
+print('{} x {} = {}'.format(tabuada, 1, tabuada*1))
+print('{} x {} = {}'.format(tabuada, 2, tabuada*2))
+print('{} x {} = {}'.format)
